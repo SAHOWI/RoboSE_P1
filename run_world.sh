4f23a5150d05d6df9855d3c8df0c01787c12b7d5
SAHOWI <wienecke.ch@gmail.com>
2020-11-26 22:22:22 +0000

    fixed run_world.sh script

--- run_world.sh
+++ run_world.sh
@@ -5,6 +5,15 @@ mkdir build
 cd build/
 cmake ../
 make
+echo 'build complete, ensure to add'
+echo '   <plugin name="welcome_message" filename="libwelcome_message.so"/>'
+echo 'to your world/my_world description after'
+echo '   <world name="default">'
+echo 
+echo you need to 
+echo    export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:${MY_WORLD}
+echo before you can start gazebo with
+echo    cd world
+echo    gazebo my_world 
 export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:${MY_WORLD}
-
 
